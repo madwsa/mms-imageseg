89037834e94ee11cdc5945b04283258671010a88
Spencer Drakontaidis <spencerdrak@gmail.com>
2021-11-13 10:50:35 -0800

    Broken vectorized impl of pre-processing

--- image-preprocessing/preprocess-labels.py
+++ image-preprocessing/preprocess-labels.py
@@ -4,6 +4,8 @@ import sys
 from os import listdir
 from os.path import isfile, join
 
+from numpy.core.numeric import zeros_like
+
 LABEL_COLORS = np.array([
     [255,0,0], #Solar Panels
     [0,255,0], #Drive Shaft
@@ -17,6 +19,13 @@ LABEL_COLORS = np.array([
     [64,128,192], #Launch Vehicle Adapter
     [0,0,0] #Black
 ])
+
+LABEL_COLOR_INDEX_MAP = {}
+
+for i in range(len(LABEL_COLORS)):
+    LABEL_COLOR_INDEX_MAP[(LABEL_COLORS[i][0], LABEL_COLORS[i][1], LABEL_COLORS[i][2])] = i
+
+print(LABEL_COLOR_INDEX_MAP)
 
 def closest(color, label_colors):
     """Find the closest color.
@@ -36,27 +45,7 @@ def closest(color, label_colors):
     smallest_distance = label_colors[index_of_smallest]
     return smallest_distance[0], index_of_smallest[0]
 
-def filter_black(pixel, epsilon):
-    """Convert an almost black pixel to black.
-
-    pixel : np.array of shape (3)
-    epsilon : float
-        the percent "error" representing how far away from black a given RGB value can be.
-
-    Returns the (pixel, True) if the pixel is now true black, (pixel, False) otherwise
-
-    This operates on each channel separately.
-
-    """
-
-    comp = pixel - epsilon
-
-    if comp[0] > 0 or comp[1] > 0 or comp[2] > 0:
-        return (pixel, False)
-    else:
-        return (np.array([0,0,0]), True)
-
-def process_image(img_path, output_dir, label_colors, epsilon = np.array([25,25,25])):
+def process_image(img_path, output_dir, label_colors, epsilon = 10):
     """Standardize colors in images and make all nearly-black into black.
 
     img_path : path to the img in question
@@ -65,32 +54,46 @@ def process_image(img_path, output_dir, label_colors, epsilon = np.array([25,25,
     epsilon : float
         the percent "error" representing how far away from black a given RGB value can be. 
 
-    Source: https://stackoverflow.com/questions/138250/how-to-read-the-rgb-value-of-a-given-pixel-in-python
-
+    Sources: 
+    - https://stackoverflow.com/questions/138250/how-to-read-the-rgb-value-of-a-given-pixel-in-python
+    - https://scikit-image.org/docs/dev/user_guide/numpy_images.html
     """
 
-    new_image = Image.new('RGB', (800, 600))
+    img = Image.open(img_path).convert('RGB')
+    arr = np.array(img)
 
-    with Image.open(img_path) as img:
-        pix = img.load()
-        if img.mode != 'RGB':
-            print("Image Mode not RGB.")
-            sys.exit(1)
-        width, height = img.size
-        if width != 800 or height != 600:
-            print(f"Image Dimensions incorrect. Width was {width} and Height was {height}.")
-        grayscale_array = np.zeros((height, width), dtype=np.uint8)
-        for x in range(0,width):
-            for y in range(0,height):
-                pixel = np.array(pix[x,y])
-                closest_color,label_color_index = closest(pixel, label_colors)
-                grayscale_array[y,x] = label_color_index
+    zero_array = np.zeros((3))
 
-    grayscale_img = Image.fromarray(grayscale_array)
+    blackish = arr[:, :, :] <= np.array((epsilon,epsilon,epsilon))
 
-    file_part = img_path.split("/")[-1]
-    new_filename = f"{output_dir}/{file_part}"
-    grayscale_img.save(new_filename)
+    arr[blackish] = 0
+
+    f = lambda p: LABEL_COLOR_INDEX_MAP[p[0],p[1],p[2]]
+
+    out = np.apply_along_axis(f, 2, arr)
+
+    print(out)
+
+    # with Image.open(img_path) as img:
+    #     pix = img.load()
+    #     if img.mode != 'RGB':
+    #         print("Image Mode not RGB.")
+    #         sys.exit(1)
+    #     width, height = img.size
+    #     if width != 800 or height != 600:
+    #         print(f"Image Dimensions incorrect. Width was {width} and Height was {height}.")
+    #     grayscale_array = np.zeros((height, width), dtype=np.uint8)
+    #     for x in range(0,width):
+    #         for y in range(0,height):
+    #             pixel = np.array(pix[x,y])
+    #             closest_color,label_color_index = closest(pixel, label_colors)
+    #             grayscale_array[y,x] = label_color_index
+
+    # grayscale_img = Image.fromarray(grayscale_array)
+
+    # file_part = img_path.split("/")[-1]
+    # new_filename = f"{output_dir}/{file_part}"
+    # grayscale_img.save(new_filename)
 
 def enumerate_images(input_dir):
     """Get a path to all files in the specified dir.
@@ -105,8 +108,6 @@ if __name__ == "__main__":
     IMG_DIR = sys.argv[1]
     OUTPUT_DIR = sys.argv[2]
     input_imgs = enumerate_images(IMG_DIR)
-    epsilon_float_val = float(sys.argv[3])
-
-    EPSILON = np.array([epsilon_float_val, epsilon_float_val, epsilon_float_val])
+    EPSILON = int(sys.argv[3])
     for img in input_imgs:
         process_image(img, OUTPUT_DIR, LABEL_COLORS, EPSILON)
